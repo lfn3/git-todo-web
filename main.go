cde421e6f0d8d6b9c5e698afabb47e1e30edaf1d
byAtlas <atlas@byatlas.com>
2014-03-23 20:41:10 +1300

    Working file listing template

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ type RenderableRepo struct {
 }
 
 type TrackedFile struct {
-	fileName string
+	FileName string
 }
 
 func main() {
@@ -63,9 +63,12 @@ func main() {
 	m.Use(render.Renderer())
 
 	m.Get("/", func(router martini.Routes, logger *log.Logger, r render.Render) {
-		model := RenderableRepoList{route: router}
+		model := RenderableRepoList{
+			Repos: make([]RenderableRepo, len(repos)),
+			TemplateModel: TemplateModel{ Route: router },
+		}
 		for repoName, repo := range repos {
-			rRepo := RenderableRepo{repoName: repoName}
+			rRepo := RenderableRepo{RepoName: repoName}
 
 			branch, err := repo.LookupBranch("master", git.BranchLocal)
 			if err != nil {
@@ -76,20 +79,20 @@ func main() {
 				logger.Println(err)
 			}
 			currentTree, err := currentCommit.Tree()
-			rRepo.Files :=
+			rRepo.Files = make([]TrackedFile, currentTree.EntryCount())
 			for i := uint64(0); i < currentTree.EntryCount(); i++ {
 				treeEntry := currentTree.EntryByIndex(i)
 				if err != nil {
 					logger.Println(err)
 				}
 
-				output += "<li><a href=\"" + router.URLFor("file", repoName, treeEntry.Name) + "\"> " + treeEntry.Name + "</a></li>"
+				rRepo.Files = append(rRepo.Files, TrackedFile{FileName: treeEntry.Name})
 			}
 
-			output += "</ul>"
+			model.Repos = append(model.Repos, rRepo)
 		}
 
-		r.HTML(200, "fileList", RenderableRepoList)
+		r.HTML(200, "fileList", model)
 	}).Name("index")
 
 	m.Get("/repo/:repoName/file/:fileName", func(params martini.Params, logger *log.Logger) string {
